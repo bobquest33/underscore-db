e49f9533a01f255534702c1acbd43ba221bafa79
Typicode <typicode@gmail.com>
2014-01-13 15:43:06 +0100

    First commit

---
+++ db.js
@@ -0,0 +1,10 @@
+var db = {
+  posts: [
+    {id: 1, body: 'foo', published: false},
+    {id: 2, body: 'bar', published: true}
+  ],
+  comments: [
+    {id: 1, body: 'baz', postId: 1},
+    {id: 2, body: 'qux', postId: 2}
+  ]
+}
