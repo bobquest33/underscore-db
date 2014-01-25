61f3a4f27b25bd11ee6d324358cc1edd28f638a5
Typicode <typicode@gmail.com>
2014-01-25 06:35:07 +0100

    Update project site

--- db.js
+++ db.js
@@ -1,10 +1,15 @@
 var db = {
   posts: [
-    {id: 1, body: 'foo', published: false},
-    {id: 2, body: 'bar', published: true}
+    {id: 1, body: 'Post one', published: true},
+    {id: 2, body: 'Post two', published: true},
+    {id: 3, body: 'Post three', published: true},
+    {id: 4, body: 'Post four', published: true},
+    {id: 5, body: 'Post five', published: false},
+    {id: 6, body: 'Post six', published: false},
+    {id: 7, body: 'Post seven', published: false}
   ],
   comments: [
-    {id: 1, body: 'baz', postId: 1},
-    {id: 2, body: 'qux', postId: 2}
+    {id: 1, body: 'Comment one', postId: 1},
+    {id: 2, body: 'Comment two', postId: 2}
   ]
 }
